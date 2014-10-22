96ebfd957697fe692294fae6ce6f4480405ade2f
ghbhaha <ghbhaha@gmail.com>
2014-10-22 16:27:46 +0800

    rm auto_make.sh&&add Fullota.sh&&usage: ./Fullota.sh 4.8.21

---
+++ Fullota.sh
@@ -0,0 +1,33 @@
+#usage: ./Fullota.sh 4.8.21
+# !/bin/sh
+OTAPATH=otapath
+DATE=$1
+MODEL=A0001
+randstr() {
+  index=0
+  str=""
+  for i in {a..z}; do arr[index]=$i; index=`expr ${index} + 1`; done
+  for i in {0..9}; do arr[index]=$i; index=`expr ${index} + 1`; done
+  for i in {0..9}; do arr[index]=$i; index=`expr ${index} + 1`; done
+  for i in {1..10}; do str="$str${arr[$RANDOM%$index]}"; done
+  echo $str
+}
+
+RANDSRT=`randstr`
+
+echo 正在编译miui_"$MODEL"_suda_"$DATE"_"$RANDSRT"_4.4.4.zip
+echo 请耐心等候。。。
+cd ..
+. build/envsetup.sh 
+cd -
+make clean
+make fullota BUILD_NUMBER=$DATE
+mv  out/fullota.zip ./miui_"$MODEL"_suda_"$DATE"_"$RANDSRT"_4.4.4.zip
+if [ ! -d $OTAPATH ];
+then
+mkdir otapath
+fi
+mv  out/target_files.zip ./otapath/$DATE.zip
+echo 编译完成，谢谢使用
+
+
